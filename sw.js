880aa6ccd77e8a3678337a105ed3041c0452e620
energiecraftonline-afk <energiecraftonline-afk@users.noreply.github.com>
2026-07-07 06:28:05 +0200

    Catalogue ADN VF : 153 nouveaux animes doubles + 44 liens ADN ajoutes aux existants (total 361)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Anime Tracker VF - Offline Caching
-const CACHE_NAME = 'anime-tracker-vf-v4';
+const CACHE_NAME = 'anime-tracker-vf-v5';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
